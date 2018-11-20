d41a350f4010ce5e8c072b395acec7cda353629b
Sterling Walker <sterling.walker@jet.com>
2018-11-19 23:24:21 -0500

    Add connection logic

---
+++ reports.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#
+# Prints reports
+
+import psycopg2
+
+def connect():
+    try:
+        db = psycopg2.connect("dbname=news")
+        cursor = db.cursor()
+        print ("connected")
+        db.close()
+        print ("closed!")
+    except:
+        print ("Not connected")
+
+
+if __name__ == '__main__':
+    connect()
